da1765a5c5eb74b172baa68202b7ff2b61ecdfca
lucasvleal <lucasleal.dev@gmail.com>
2022-10-25 13:38:05 -0300

    lint: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import "./styles/global.css";
-import Home from "./pages/Home";
+import React from 'react'
+import './styles/global.css'
+import Home from './pages/Home'
 
-const App: React.FC = () => <Home />;
+const App: React.FC = () => <Home />
 
-export default App;
+export default App
